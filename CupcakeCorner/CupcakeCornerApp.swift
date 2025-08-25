58b4408a1d99123cab17e1ec846d8926cd174763
Santhosh2141 <snthshsrinivas@gmail.com>
2025-08-25 19:59:33 +0530

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Santhosh Srinivas on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
